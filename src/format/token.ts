fcd7f308ae4e9d0d47b4d1d6c922da94811044f7
Percy Ma <kecrily@gmail.com>
2023-07-06 14:05:19 +0800

    feat: support fully Tokenizer

--- src/format/token.ts
+++ src/format/token.ts
@@ -1,4 +1,5 @@
 import { components } from '../types/data'
+import { compiledConvert } from './utils'
 
 export type TokenType =
   | 'text' // plain text
@@ -40,4 +41,36 @@ export function tokenPlatform(t: components['schemas']['Transfer']) {
   if (t.platform) platform = t.platform
   if ('platform' in t.metadata) platform = t.metadata.platform
   return [tokenText('on'), tokenText('platform'), token('platform', platform)]
+}
+
+export function tokenPostBrief(t: components['schemas']['Transfer']) {
+  if (t.tag !== 'social') {
+		return tokenText('')
+	}
+
+	let out = ''
+
+	if ('title' in t.metadata && t.metadata.title) {
+		out = t.metadata.title
+	} else if ('body' in t.metadata && t.metadata.body) {
+		out = t.metadata.body
+	} else if ('target' in t.metadata && t.metadata.target.body) {
+		out = t.metadata.target.body
+	}
+
+	out = compiledConvert(out)
+
+	let trimmed = false
+	const max = 50
+	if (out.length > max) {
+		out = out.slice(0, max)
+		trimmed = true
+	}
+
+	if (/\n/.test(out)) {
+		out = out.replace(/\n[\s\S]+/g, '')
+		trimmed = true
+	}
+
+	return tokenText(out + trimmed ? '...' : '')
 }
