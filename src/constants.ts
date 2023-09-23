1ae592c05712f16a0734209e9404a31f9a61441b
Henry <hi@henry.wang>
2023-09-23 13:39:23 +0100

    chore: switch to testnet.rss3.io

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_DATA_SERVER = 'https://api.rss3.io/data'
-export const DEFAULT_SEARCH_SERVER = 'https://api.rss3.io/search'
+export const DEFAULT_DATA_SERVER = 'https://testnet.rss3.io/data'
+export const DEFAULT_SEARCH_SERVER = 'https://testnet.rss3.io/search'
