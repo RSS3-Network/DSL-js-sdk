27cca38beb813388e96182335c14d0ff62f20ef5
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2023-10-06 16:23:09 +1300

    add: readable profile logic

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,21 @@
 export const DEFAULT_RSS3_MAINNET = 'https://api.dev.rss3.io'
 export const INFINITY_VALUE = '115792089237316195423570985008687907853269984665640564039457584007913129639935'
+export const SUPPORTED_NS_LIST = [
+  '.eth',
+  '.lens',
+  '.csb',
+  '.bnb',
+  '.bit',
+  '.crypto',
+  '.zil',
+  '.nft',
+  '.x',
+  '.wallet',
+  '.bitcoin',
+  '.dao',
+  '.888',
+  '.blockchain',
+  '.avax',
+  '.arb',
+  '.cyber',
+]
