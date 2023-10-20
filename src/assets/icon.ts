afd006351ca9e8f5b9f1cb9423519654e46d5fc0
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2023-10-21 01:14:28 +1300

    fix: lint

--- src/assets/icon.ts
+++ src/assets/icon.ts
@@ -1,4 +1,4 @@
-import { Network, Tag } from '../data/client'
+import { Network, Tag } from '../data/client.js'
 
 export function getIcon(name: Tag | Network | string) {
   return `https://assets.rss3.io/web3-icons/${name}.png`
