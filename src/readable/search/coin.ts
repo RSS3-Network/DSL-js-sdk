e67739afa609cc4e726be9804d1be588d813b58b
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2023-10-25 23:26:06 +1300

    add: day options

--- src/readable/search/coin.ts
+++ src/readable/search/coin.ts
@@ -1,3 +1,30 @@
+export const DAY_OPTIONS = [
+  {
+    name: '1D',
+    value: '1',
+  },
+  {
+    name: '7D',
+    value: '7',
+  },
+  {
+    name: '1M',
+    value: '30',
+  },
+  {
+    name: '3M',
+    value: '90',
+  },
+  {
+    name: '1Y',
+    value: '365',
+  },
+  {
+    name: 'ALL',
+    value: 'max',
+  },
+]
+
 export function formatLink(arr: string[]): string {
   return arr.find((i) => !!i) || ''
 }
